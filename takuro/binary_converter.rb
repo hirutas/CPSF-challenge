dc8be1c709a0e5cfd52cd595f94f83a421af5815
Shinya Hiruta <hiru@ht.sfc.keio.ac.jp>
2011-10-13 17:57:15 +0900

    エンコード・デコードに対応

---
+++ takuro/binary_converter.rb
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+require 'optparse'
+
+# mode flag
+is_encode = false
+is_decode = false
+aes_key = nil
+OptionParser.new do |opt|
+  opt.on("-e"){|v| is_encode = v }
+  opt.on("-d"){|v| is_decode = v }
+  opt.on("-k AESKEY"){|v| aes_key = v }
+  opt.parse!(ARGV)
+end
+
+# switch mode
+if(is_encode && is_decode)
+  puts "invalid option -e (encode) and -d (decode)"
+  exit -1
+elsif(!is_encode && !is_decode)
+  puts "select option -e (encode) or -d (decode)"
+  exit -1
+# encode mode (bit -> text)
+elsif(is_encode)
+  input = ""
+  while line = $stdin.gets
+    line.chomp.each_char{|c| 
+      printf("%08b", c[0])
+    }
+    puts    
+  end
+# decode mode (text -> bit)
+elsif(is_decode)
+  while line = $stdin.gets
+    puts [line.chomp].pack("B*")
+  end  
+else
+  puts "must not happen!"
+  exit -1
+end
