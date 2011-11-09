c3af06835ad1d696d7ceaf810f854ab5f586628b
Shinya Hiruta <hiru@ht.sfc.keio.ac.jp>
2011-11-09 19:55:48 +0900

    描画系の機能を一通り実装

--- hiru/htdocs/js/main.js
+++ hiru/htdocs/js/main.js
@@ -1,39 +1,43 @@
 // オブジェクトを1つだけグローバルな名前空間に追加
 new function(){
+	// private variables
+	// var hoge = "huga";
 	myCanvasObj = {
 		// public variables
 		mouseX: 0,
 		mouseY: 0,
+		jqBody: null,
 		jqCanvas: null,
 		domCanvas: null,
 		ctx: null,
 		isMouseDown: false,
 		// public functions
-		initialize: function(jqc){
+		initialize: function(jqb, jqc){
 			// set canvas objects
+			this.jqBody = jqb;
 			this.jqCanvas = jqc;
 			this.domCanvas = this.jqCanvas.get(0);
 			this.ctx = this.domCanvas.getContext('2d');
 			// set mouse events
-			this.jqCanvas.mousedown(function(e){
+			this.jqBody.mousedown(function(e){
 				myCanvasObj.mouseX = e.pageX - this.offsetLeft; // コールバック関数内ではthisは使えない
 				myCanvasObj.mouseY = e.pageY - this.offsetTop;
-				myCanvasObj.isMouseDown = true;
 				myCanvasObj.ctx.beginPath();
 				myCanvasObj.ctx.moveTo(this.mouseX, this.mouseY);
+				myCanvasObj.isMouseDown = true;
 				console.log('mousedown');
 			});
-			this.jqCanvas.mouseup(function(e){
-				myCanvasObj.mouseX = e.pageX - this.offsetLeft;
-				myCanvasObj.mouseY = e.pageY - this.offsetTop;
+			this.jqBody.mouseup(function(e){
 				myCanvasObj.isMouseDown = false;
 				console.log('mouseup');
 			});
 			this.jqCanvas.mouseout(function(e){
-				myCanvasObj.mouseX = e.pageX - this.offsetLeft;
-				myCanvasObj.mouseY = e.pageY - this.offsetTop;
-				myCanvasObj.isMouseDown = false;
 				console.log('mouseout');
+			});
+			this.jqCanvas.mouseover(function(e){
+				myCanvasObj.ctx.beginPath();
+				myCanvasObj.ctx.moveTo(this.mouseX, this.mouseY);
+				console.log('mouseover');
 			});
 			this.jqCanvas.mousemove(function(e){
 				myCanvasObj.mouseX = e.pageX - this.offsetLeft;
@@ -48,13 +52,56 @@ new function(){
 				this.ctx.stroke();
 				console.log(this.mouseX + ', ' + this.mouseY);
 			}
+		},
+		changeColor: function(hex){
+			this.ctx.strokeStyle = '#' + hex;
+		},
+		changeWidth: function(width){
+			this.ctx.lineWidth = width;
+		},
+		saveImage: function(){
+			var img = new Image();
+			var type = 'image/png';
+			img.src = this.domCanvas.toDataURL(type);
+			img.onload = function(){
+				location.href = img.src;
+			}
 		}
 	};
 } // コンストラクタを即時実行する
 
 // onLoad
 $(function(){
-	myCanvasObj.initialize($('canvas#main'));
+	myCanvasObj.initialize($('body'), $('canvas#main'));
+	$('#colorPicker').ColorPicker({
+		flat: true,
+		color: '#000000',
+		onChange: function(hsb, hex, rgb) {
+			console.log(hex);
+			myCanvasObj.changeColor(hex);
+		}
+	});
+	
+	var resetSelectWidth = function(){
+		$('#width1').css('border', 'solid 1px #bbb');
+		$('#width3').css('border', 'solid 1px #bbb');
+		$('#width5').css('border', 'solid 1px #bbb');
+	};
+	$('#width1').click(function(){
+		myCanvasObj.changeWidth(1);
+		resetSelectWidth();
+		$(this).css('border', 'solid 1px #ff0000');
+	});
+	$('#width3').click(function(){
+		myCanvasObj.changeWidth(3);
+		resetSelectWidth();
+		$(this).css('border', 'solid 1px #ff0000');
+	});
+	$('#width5').click(function(){
+		myCanvasObj.changeWidth(5);
+		resetSelectWidth();
+		$(this).css('border', 'solid 1px #ff0000');
+	});
 	
 });
 
