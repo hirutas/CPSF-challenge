19190014691a69dae916fb99d575a9f479163c8d
Shinya Hiruta <hiru@ht.sfc.keio.ac.jp>
2011-11-11 19:45:08 +0900

    showからTOPに戻るリンクを分かりやすくした

--- hiru/htdocs/show.php
+++ hiru/htdocs/show.php
@@ -36,6 +36,7 @@ if(array_key_exists('id', $_GET)){
 	<?php if($url): ?>
 	<a href="https://twitter.com/share" class="twitter-share-button" data-count="horizontal" data-via="hiru_ecn" data-lang="ja">ツイート</a><script type="text/javascript" src="//platform.twitter.com/widgets.js"></script>
 	<?php endif; ?>
+	<h2>&lt;&lt; <a href="/oekaki">お絵かきする</a></h2>
 	</div>
   <footer>
     <a href="http://www.ht.sfc.keio.ac.jp/cpsf/" target="_blank">CPSF</a> - Cyber-Physical Space Family in Keio Univ. Hide Tokuda Lab.<br />
